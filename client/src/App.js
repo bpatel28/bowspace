48b22de70ef290d0765c83a54d30f4982f2192f6
Brijesh Patel <b.patel9528@gmail.com>
2019-05-30 18:59:22 -0600

    stateless component login created

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import Login from './components/Login'
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import {  } from 'react-bootstrap'
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {};
   }
   
   //render app
   render() {
     return (
-      <div></div>
+      <Router>
+        <Route path="/login" component={Login}/>
+        <Route path="/register" component={Login} />
+      </Router>
     );
   }
 }
---
+++ client/src/components/Login.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {Link} from "react-router-dom"
+import {Container, Row, Col, Form, Button, Card} from "react-bootstrap";
+
+const Login = (props) => {
+    return (
+        <div className='bg-dark d-flex align-items-center' style={{height : '100vh', backgroundColor : 'black'}}>
+          <Container>
+            <Row>
+              <Col></Col>
+              <Col md="5">
+                    <Card border='primary'>
+                        <Card.Header as='h5' className="text-center text-white" style={{ backgroundColor : '#3575dd' }}>
+                            Login
+                        </Card.Header>
+                        <Card.Body>
+                                <Form>
+                                    <Form.Group controlId="formBasicEmail">
+                                        <Form.Label>Email address</Form.Label>
+                                        <Form.Control type="email" placeholder="Enter email" />
+                                    </Form.Group>
+
+                                    <Form.Group controlId="formBasicPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" placeholder="Password" />
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Form.Label className="text-muted text-center">
+                                            Don't have an account. <Link to='/'> Register Now. </Link>
+                                        </Form.Label>
+                                    </Form.Group>
+                            
+                                    <Form.Group className="text-center">
+                                            <Button style={{backgroundColor : '#3575dd'}} className="text-center" type="submit">
+                                                Submit
+                                            </Button>
+                                    </Form.Group>
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col></Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default Login; 
